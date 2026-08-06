3996f1a34738092bdd08df57672fd80c85947190
Owais <contact@owais.fi>
2026-08-06 14:36:32 +0300

    add junction tables

--- migrations/0000001_init_schema.up.sql
+++ migrations/0000001_init_schema.up.sql
@@ -1,21 +1,44 @@
-CREATE TABLE IF NOT EXISTS genres(
+CREATE TABLE IF NOT EXISTS genres (
 
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL UNIQUE
 
 );
 
-CREATE TABLE IF NOT EXISTS movies(
+CREATE TABLE IF NOT EXISTS movies (
 
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     release_year INTEGER NOT NULL,
-    duration INTEGER NOT NULL,
+    duration INTEGER NOT NULL
+    
 );
 
-CREATE TABLE IF NOT EXISTS actors(
+CREATE TABLE IF NOT EXISTS actors (
     
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    birth_date TEXT NOT NULL,
+    birth_date TEXT NOT NULL
+
+);
+
+CREATE TABLE IF NOT EXISTS movie_genres (
+    
+    movie_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL,
+    PRIMARY KEY (movie_id, genre_id),
+    FOREIGN KEY (movie_id) REFERENCES movies(id),
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
+
+);
+
+
+CREATE TABLE IF NOT EXISTS movie_actors (
+    
+    movie_id INTEGER NOT NULL,
+    actor_id INTEGER NOT NULL,
+    PRIMARY KEY (movie_id, actor_id),
+    FOREIGN KEY (movie_id) REFERENCES movies(id),
+    FOREIGN KEY (movie_id) REFERENCES actors(id)
+
 );
