49d3016f6adb06031480fa66603cfd3346a22579
Owais <contact@owais.fi>
2026-08-06 15:41:55 +0300

    Add database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"log"
+	"movies-api/internal/database"
 )
 
 func main(){
 
 	fmt.Println("movies-api")
 
+	err := os.MkdirAll("./data", 0755)
+	if err != nil {
+		log.Fatalf("Failed to create directory: %v", err)
+	}
+
+	dbPath := "./data/movies.db"
+	db, err := database.New(dbPath)
+	if err != nil {
+		log.Fatalf(">> ERROR: %v", err)
+	}
+
+	defer db.Close()
+	
 }
