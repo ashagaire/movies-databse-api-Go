7e4dbb6b74931de449c9d4fc00b9e655edece360
Owais Javed <contact@owais.fi>
2026-08-07 14:41:01 +0300

    Update struct for N-M relationships

--- internal/models/models.go
+++ internal/models/models.go
@@ -5,15 +5,17 @@ type Genre struct {
 	Name string `json:"name"`
 }
 
-type Movie struct {
-	ID          int64  `json:"id"`
-	Title       string `json:"title"`
-	ReleaseYear int    `json:"releaseYear"`
-	Duration    int    `json:"duration"`
-}
-
 type Actor struct {
 	ID        int64  `json:"id"`
 	Name      string `json:"name"`
 	BirthDate string `json:"birthDate"`
+}
+
+type Movie struct {
+	ID          int64   `json:"id"`
+	Title       string  `json:"title"`
+	ReleaseYear int     `json:"releaseYear"`
+	Duration    int     `json:"duration"`
+	Genres      []Genre `json:"genres,omitempty"`
+	Actors      []Actor `json:"actors,omitempty"`
 }
