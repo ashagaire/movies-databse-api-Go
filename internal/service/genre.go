ded90802659fb5f7f652274dc01e68bdbdf1779d
Owais <contact@owais.fi>
2026-08-09 14:36:45 +0300

    Add Create(), GetAll(), GetByID(), Update(), Delete() methods

---
+++ internal/service/genre.go
@@ -0,0 +1,70 @@
+package service
+
+import (
+	"fmt"
+	"movies-api/internal/repository"
+)
+
+type GenreService struct {
+	repo *repository.GenreRepository
+}
+
+func NewGenreService(repo *repository.GenreRepository) *GenreService {
+	return &GenreService{
+		repo: repo
+	}
+}
+
+func (s *GenreService) Create(genre *models.Genre) error {
+	
+	genre.Name = strings.TrimSpace(genre.Name)
+	if genre.Name == "" {
+		return errors.New("genre name cannot be empty")
+	} 
+	
+	err := s.repo.Create(genre)
+	if err != nil {
+		return fmt.Errorf("service failed to create genre: %w", err)
+	}
+
+	return nil
+}
+
+func (s *GenreService) GetAll() ([]models.Genre, error) {
+	return s.repo.GetAll()
+}
+
+func (s *GenreService) GetByID(id int64) (models.Genre, error) {
+	if id <= 0 {
+		return models.Genre{}, errors.New("invalid ID provided")
+	}
+	return s.repo.GetByID(id)
+}
+
+func (s *GenreService) Update(genre *models.Genre) error {
+	
+	genre.Name = strings.TrimSpace(genre.Name)
+	
+	if genre.Name == "" {
+		return errors.New("genre name cannot be empty")
+	}
+	if genre.ID <= 0 {
+		return errors.New("invalid ID provided")
+	}
+
+	return s.repo.Update(genre)
+}
+
+
+func (s *GenreService) Delete(id int64, force bool) error {
+	
+	if id <= 0 {
+		return errors.New("invalid ID provided")
+	}
+
+	if force == true {
+		fmt.Println("Force delete requested!")
+	}
+
+	return s.repo.Delete(id)
+}
