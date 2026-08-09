b982867c7c441eadf4f111327602766e85817c52
Owais <contact@owais.fi>
2026-08-09 13:42:42 +0300

    Add Update(), Delete() Methods

--- internal/repository/genre.go
+++ internal/repository/genre.go
@@ -70,3 +70,46 @@ func (r *GenreRepository) GetAll() ([]models.Genre, error){
 	return genres, nil
 
 }
+
+func (r *GenreRepository) GetByID(id int64) (*models.Genre, error) {
+
+	query := `SELECT id, name FROM genres WHERE id = ?`
+
+	var g *models.Genre 
+
+	err := r.db.QueryRow(query, id).Scan(&g.ID, &g.Name)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("genre with ID %d not found", id)
+		}
+		return nil, fmt.Errorf("failed to query genre: %w", err)
+	}
+
+	return &g, nil
+}
+
+
+func (r *GenreRepository) Update(genre *models.Genre) error {
+	query := `UPDATE genres SET name = ? WHERE id = ?`
+	
+	_, err := r.db.Exec(query, genre.Name, genre.ID)
+	if err != nil {
+		return fmt.Errorf("failed to update genre: %w", err)
+	}
+
+	return nil
+}
+
+func (r *GenreRepository) Delete(id int64) error {
+	
+	query := `DELETE FROM genres WHERE id = ?`
+	
+	_, err := r.db.Exec(query, id)
+	if err != nil {
+		return fmt.Errorf("failed to delete genre: %w", err)
+	}
+
+	return nil
+	
+}
