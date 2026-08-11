406638454ef62d85f2e0b6030f89d620fce9d519
Owais <contact@owais.fi>
2026-08-11 14:40:00 +0300

    ActorRepository: Add GetAll() method

--- internal/repository/actor.go
+++ internal/repository/actor.go
@@ -10,13 +10,13 @@ type ActorRepository struct {
 	db *sql.DB
 }
 
-func NewActorRepository(db *sql.DB) *ActorRepository {
+func NewActorRepository(db *sql.DB) *ActorRepository{
 	return &ActorRepository{
 		db: db,
 	}
 }
 
-func (r *ActorRepository) Create(actor *models.Actor) error {
+func (r *ActorRepository) Create(actor *models.Actor) error{
 
 	query := `INSERT INTO actors (name, birth_date) VALUES (?, ?)`
 
@@ -33,4 +33,37 @@ func (r *ActorRepository) Create(actor *models.Actor) error {
 	actor.ID = id
 
 	return nil
+}
+
+func (r *ActorRepository) GetAll()([]models.Actor, error){
+
+	query := `SELECT id, name, birth_date FROM actors`
+
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query actors: %w", err)
+	}
+
+	defer rows.Close()
+
+	var actors []models.Actor
+	
+	for rows.Next(){
+		
+		var a models.Actor
+
+		err := rows.Scan(&a.ID, &a.Name, &a.BirthDate)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan actor: %w", err)
+		}
+
+		rows = append(actors, a)
+	}
+	
+	err = rows.Err();
+	if err != nil {
+		return nil, fmt.Errorf("row iteration error: %w", err)
+	}
+
+	return actors, nil
 }
