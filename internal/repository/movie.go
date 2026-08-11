ca124ea4b91ab7e7caf0de5db9332fdd924b5253
Owais <contact@owais.fi>
2026-08-11 15:32:39 +0300

    MovieRepository: Update Create() method

--- internal/repository/movie.go
+++ internal/repository/movie.go
@@ -27,12 +27,32 @@ func (r *MovieRepository) Create(movie *models.Movie) error {
 		return fmt.Errorf("failed to enter movie: %w", err)
 	}
 
-	id, err := result.LastInsertId()
+	movieID, err := result.LastInsertId()
 	if err != nil {
-		return fmt.Errorf("failed to get id: %w", err)
+		return fmt.Errorf("failed to get movieID: %w", err)
 	}
 
-	movie.ID = id
+	movie.ID = movieID
+
+	for _, genre := range movie.Genres(
+		
+		genreQuery := `INSERT INTO movie_genres (movie_id, genre_id) VALUES (?, ?)`
+		_, err := db.Exec(genreQuery, movie.ID, genre.ID)
+		if err != nil{
+			return fmt.Errorf("failed to link genre ID %d: %w", genre.ID, err)
+		}
+
+	)
+
+	for _, actor := range movie.Actors(
+
+		actorQuery := `INSERT INTO movie_actors (movie_id, actor_id) VALUES (?, ?)`
+		_, err := db.Exec(actorQuery, movie.ID, actor.ID)
+		if err != nil{
+			return fmt.Errorf("failed to link actor ID %d: %w", actor.ID, err)
+		}
+
+	)
 
 	return nil
 
