f6bc77c4a94b96aaaac009b921c33827a59a08ee
Owais <contact@owais.fi>
2026-08-22 13:56:07 +0300

    Movie Servie > Create() method

--- internal/service/movie.go
+++ internal/service/movie.go
@@ -17,6 +17,26 @@ func NewMovieService(repo *repository.MovieRepository) *MovieService {
 	return &MovieService{repo: repo}
 }
 
-func (s *MovieService) Create(){
+func (s *MovieService) Create(movie *models.Movie) error {
 	
+	movie.Title = strings.TrimSpace(movie.Title)
+	if movie.Title == "" {
+		return errors.New("movie title cannot be empty")
+	}
+
+	if movie.ReleaseYear < 1888 {
+		return errors.New("release year must be 1888 or later")
+	}
+
+	if movie.Duration <= 0 {
+		return errors.New("duration must be greater than 0 minutes")
+	}
+
+	err := s.repo.Create(movie)
+	if err != nil {
+		return fmt.Errorf("failed to create movie: %w", err)
+	}
+
+	return nil
 }
+
