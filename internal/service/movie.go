4042023bc6a170ce2160727e4532132f1a7cbf0e
Owais <contact@owais.fi>
2026-08-22 13:53:53 +0300

    Servie > Add movie.go

---
+++ internal/service/movie.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"movies-api/internal/models"
+	"movies-api/internal/repository"
+)
+
+type MovieService struct {
+	repo *repository.MovieRepository
+}
+
+func NewMovieService(repo *repository.MovieRepository) *MovieService {
+	return &MovieService{repo: repo}
+}
+
+func (s *MovieService) Create(){
+	
+}
