3d804eb3d9a9f430cbf788f60cb367f7314af111
Owais <contact@owais.fi>
2026-08-22 13:46:16 +0300

    DEBUG: Add movies schema

--- internal/service/actor_test.go
+++ internal/service/actor_test.go
@@ -16,14 +16,23 @@ func setupTestDB(t *testing.T) *sql.DB {
 	}
 
 	schema := `
-		CREATE TABLE actors (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, birth_date TEXT NOT NULL);
+		CREATE TABLE IF NOT EXISTS movies (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			title TEXT NOT NULL,
+			release_year INTEGER NOT NULL,
+			duration INTEGER NOT NULL
+		);
+		CREATE TABLE actors (
+			id INTEGER PRIMARY KEY AUTOINCREMENT, 
+			name TEXT NOT NULL, 
+			birth_date TEXT NOT NULL
+		);
 		CREATE TABLE IF NOT EXISTS movie_actors (    
-		movie_id INTEGER NOT NULL,
-		actor_id INTEGER NOT NULL,
-		PRIMARY KEY (movie_id, actor_id),
-		FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE RESTRICT,
-		FOREIGN KEY (actor_id) REFERENCES actors(id) ON DELETE RESTRICT
-
+			movie_id INTEGER NOT NULL,
+			actor_id INTEGER NOT NULL,
+			PRIMARY KEY (movie_id, actor_id),
+			FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE RESTRICT,
+			FOREIGN KEY (actor_id) REFERENCES actors(id) ON DELETE RESTRICT
 	);
 	`
 	_, err = db.Exec(schema)
