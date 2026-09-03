9c5039fcf36551fc1f85d34cfc518189b13adfdb
Owais Javed <contact@owais.fi>
2026-09-03 15:59:49 +0300

    FIX: Add ForceDelete()

--- internal/service/genre.go
+++ internal/service/genre.go
@@ -1,11 +1,11 @@
 package service
 
 import (
-	"fmt"
-	"strings"
 	"errors"
-	"movies-api/internal/repository"
+	"fmt"
 	"movies-api/internal/models"
+	"movies-api/internal/repository"
+	"strings"
 )
 
 type GenreService struct {
@@ -19,12 +19,12 @@ func NewGenreService(repo *repository.GenreRepository) *GenreService {
 }
 
 func (s *GenreService) Create(genre *models.Genre) error {
-	
+
 	genre.Name = strings.TrimSpace(genre.Name)
 	if genre.Name == "" {
 		return errors.New("genre name cannot be empty")
-	} 
-	
+	}
+
 	err := s.repo.Create(genre)
 	if err != nil {
 		return fmt.Errorf("service failed to create genre: %w", err)
@@ -45,9 +45,9 @@ func (s *GenreService) GetByID(id int64) (*models.Genre, error) {
 }
 
 func (s *GenreService) Update(genre *models.Genre) error {
-	
+
 	genre.Name = strings.TrimSpace(genre.Name)
-	
+
 	if genre.Name == "" {
 		return errors.New("genre name cannot be empty")
 	}
@@ -58,15 +58,14 @@ func (s *GenreService) Update(genre *models.Genre) error {
 	return s.repo.Update(genre)
 }
 
-
 func (s *GenreService) Delete(id int64, force bool) error {
-	
+
 	if id <= 0 {
 		return errors.New("invalid ID provided")
 	}
 
 	if force == true {
-		fmt.Println("Force delete requested!")
+		return s.repo.ForceDelete(id)
 	}
 
 	return s.repo.Delete(id)
