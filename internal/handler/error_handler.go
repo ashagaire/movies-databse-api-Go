85e25a7fd296d3895be022f8fe3315d542c89817
Owais <contact@owais.fi>
2026-09-05 15:54:16 +0300

    UPDATE: Add Global Error handling

---
+++ internal/handler/error_handler.go
@@ -0,0 +1,42 @@
+package handler
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"movies-api/internal/models"
+ )
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func HandleError(w http.ResponseWriter, err error ) {
+	var statusCode int
+	var message string
+
+	switch {
+
+	case errors.Is(err, models.ErrNotFound):
+		statusCode = http.StatusNotFound
+		message = err.Error( )
+	case errors.Is(err, models.ErrInvalidInput):
+		statusCode = http.StatusBadRequest
+		message = err.Error( )
+	case errors.Is(err, models.ErrConflict):
+		statusCode = http.StatusConflict // 409 Conflict
+		message = err.Error( )
+	
+	// For any other error (like database connection issues), return a 500
+	default:
+		
+		statusCode = http.StatusInternalServerError
+		message = err.Error( ) 
+
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(ErrorResponse{Error: message})
+}
